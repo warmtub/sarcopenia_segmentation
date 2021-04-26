e4a796b273a2c308c0c439a4a60627ab1f75330f
warmtub <warmtub0917@gmail.com>
2021-04-26 01:57:02 +0000

    move download to util and active server switch

--- sarcopenia_inference_app.py
+++ sarcopenia_inference_app.py
@@ -1,5 +1,4 @@
 import asyncio
-import boto3
 import base64
 import cv2
 import io
@@ -7,9 +6,6 @@ import logging
 import numpy as np
 import os
 import torch
-from botocore.client import Config
-from botocore.exceptions import ClientError
-from Image_Segmentation.evaluation import get_DC
 from Image_Segmentation.network import AttU_Net
 from PIL import Image
 from pydicom import dcmread
@@ -19,40 +15,9 @@ from torchvision import transforms as T
 
 logging.basicConfig(level=logging.INFO)
 
-# Download file from S3 blob, return download state(True or False) and error message if False
-def download_s3_file(endpoint, access_key, secret_key, bucket, dicom_file_path, download_dir_path='./'):
-    config = Config(signature_version="s3")
-    if endpoint and access_key and secret_key and bucket and dicom_file_path:
-        is_verify = True
-        connection = boto3.client(
-            "s3",
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key,
-            endpoint_url=endpoint,
-            config=config,
-            verify=is_verify,
-        )
-        
-        dicom_file = dicom_file_path.split('/')[-1]
-        download_file_path = os.path.join(download_dir_path, dicom_file) #os.path.join(docker_dicom_dir, dicom_file)
-
-        try:
-            connection.download_file(bucket, dicom_file_path, download_file_path)
-        except ClientError as e:
-            pass
-
-        if os.path.isfile(download_file_path):
-            logging.info(f"The file {dicom_file_path} is downloaded to {download_file_path}")
-            return True, "Success"
-        else:
-            logging.error("The file does not download from S3 blob")
-            return False, "The file does not download from S3 blob"
-    else:
-        logging.error("The API request information is not complete")
-        return False, "The API request information is not complete"
-
 class Sarcopenia_Inference_APP(object):
     def __init__(self,
+                 activate_server=True,
                  address="192.168.211.14",
                  port=5000,
                  download_path="download",
@@ -60,13 +25,15 @@ class Sarcopenia_Inference_APP(object):
                  classes_path="classes.txt",
                 ):
         # Sanic params
-        self.address = address
-        self.port = port
-        try:
-            self.app = Sanic(__name__)
-            self.app.add_route(self.inference_from_client, "/sarcopenia", methods=['POST'])
-        except SanicException:
-            self.app = Sanic.get_app(__name__)
+        self.activate_server = activate_server
+        if self.activate_server:
+            self.address = address
+            self.port = port
+            try:
+                self.app = Sanic(__name__)
+                self.app.add_route(self.inference_from_client, "/sarcopenia", methods=['POST'])
+            except SanicException:
+                self.app = Sanic.get_app(__name__)
         
         # Model
         self.n_classes = 12
@@ -117,7 +84,7 @@ class Sarcopenia_Inference_APP(object):
             return response.json(inference_response)
         dicom_file_path = os.path.join(self.download_path, dicom_file_path.split('/')[-1])
         
-        return response.json(self.inference(inference_response, dicom_file_path))
+        return response.json(self.inference(dicom_file_path, inference_response))
     
     #get list of dicom files from S3 blob
     def inference_from_list(self, file_list, endpoint=None, access_key=None, secret_key=None, bucket=None):
@@ -130,12 +97,14 @@ class Sarcopenia_Inference_APP(object):
                 inference_response_list[idx]['message'] = error_message
                 continue
             download_file_list[idx] = os.path.join(self.download_path, file_list[idx].split('/')[-1])
-            inference_response_list[idx] = self.inference(inference_response_list[idx], download_file_list[idx])
+            inference_response_list[idx] = self.inference(download_file_list[idx], inference_response_list[idx])
         
         return download_file_list, inference_response_list
     
     #inference single dicom file and generate json format 
-    def inference(self, inference_response, dicom_file):
+    def inference(self, dicom_file, inference_response=None):
+        if inference_response is None:
+            inference_response = self.empty_response
         ds = dcmread(dicom_file)
         image = ds.pixel_array
         image_h, image_w = image.shape
@@ -188,8 +157,8 @@ class Sarcopenia_Inference_APP(object):
                 shapes.append(shape_data)
 
         inference_response['version'] = '4.5.6'
-        file_name = ''.join(dicom_file.split('/')[-1].split('.')[:-1])
-        #inference_response['imagePath'] = file_name+'.png'        
+        file_name = '.'.join(dicom_file.split('/')[-1].split('.')[:-1])
+        inference_response['imagePath'] = file_name
         imageData = io.BytesIO()
         image_src.save(imageData, format='png')
         imageData = imageData.getvalue()
@@ -205,7 +174,10 @@ class Sarcopenia_Inference_APP(object):
         
     #start web server
     def start(self):
-        self.app.run(host=self.address, port=self.port)
+        if self.activate_server:
+            self.app.run(host=self.address, port=self.port)
+        else:
+            logging.error(f"Sanic server is not activated")
 
 
 
